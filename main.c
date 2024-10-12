ba7fa8fd00db3bf49ea34693bd76b67d4cc508e5
Fraol Gelana <fraolgela@gmail.com>
2024-10-12 18:39:12 +0300

    gaussian blur

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <raylib.h>
 
-#define WINDOW_H 256
-#define WINDOW_W 256
+#define WINDOW_H 640 
+#define WINDOW_W 640 
 
-Color *toGrayScale(Color *pixels, int img_w, int img_h)
+Color *toGrayScale(Color *pixels, size_t img_w, size_t img_h)
 {
   Color *grayscale = calloc(img_w * img_h, sizeof(Color));
 
@@ -29,6 +30,38 @@ Color *toGrayScale(Color *pixels, int img_w, int img_h)
   }
 
   return grayscale;
+}
+
+void gaussianBlur(Color *pixels,size_t img_w, size_t img_h){
+  float filter[3][3] = {{0.07511361,0.1238414,0.07511361},{0.1238414,0.20417996 ,0.1238414 }, {0.07511361, 0.1238414,  0.07511361}};
+  Color *blured= calloc(img_w * img_h, sizeof(Color));
+
+  for (size_t i = 0; i < img_h; ++i)
+  {
+    for (size_t j = 0; j < img_w; ++j)
+    {
+      float val = 0;
+      // loop through the nine neighbouring pixels
+      for (int k = -1; k <= 1; ++k)
+      {
+        for (int l = -1; l <= 1; ++l)
+        {
+          if (i + k < 0 || i + k >= img_h)
+            continue;
+          if (j + l < 0 || j + l >= img_w)
+            continue;
+          val += (pixels[img_w * (i + k) + (j + l)].r) * filter[1 + k][1 + l];
+        }
+      }
+      blured[img_w * i + j].r = (unsigned char)fmin(fmax(val, 0), 255);
+      blured[img_w * i + j].g = blured[img_w * i + j].r;
+      blured[img_w * i + j].b = blured[img_w * i + j].r;
+      blured[img_w * i + j].a = pixels[img_w * i + j].a; // Preserve the original alpha
+    }
+  }
+  memcpy(pixels,blured,sizeof(Color));
+  free(blured);
+
 }
 
 Color *sobel(Color *pixels, size_t img_w, size_t img_h)
@@ -67,12 +100,21 @@ Color *sobel(Color *pixels, size_t img_w, size_t img_h)
   return y;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
   InitWindow(WINDOW_W, WINDOW_H, "Sobel Operator");
   SetTargetFPS(30);
 
-  Image image = LoadImage("lena.png");
+  if(argc < 2){
+    printf("Please provide an image\n");
+
+    return 1;
+  }
+
+  char *image_path = argv[1];
+
+  Image image = LoadImage(image_path);
+
   Color *pixels = LoadImageColors(image);
   Texture2D input_texture = LoadTextureFromImage(image);
 
@@ -80,12 +122,13 @@ int main(void)
   size_t img_h = input_texture.height;
 
   Color *gray_pixels = toGrayScale(pixels, img_w, img_h);
+  gaussianBlur(gray_pixels,img_w,img_h);
   Color *edge_data = sobel(gray_pixels, img_w, img_h);
   // Create an Image from the Color array
   Image edge_image = {
       .data = edge_data,                          // Pointer to pixel data (array of Color)
-      .width = WINDOW_W,                          // Image width
-      .height = WINDOW_H,                         // Image height
+      .width = img_w,                          // Image width
+      .height = img_h,                         // Image height
       .mipmaps = 1,                               // Number of mipmaps (1 for no mipmaps)
       .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 // Pixel format (32-bit RGBA)
   };
